9df5de56dbcc1f007302ea62a86de560ed36923f
Bardin08 <vbardin810@gmail.com>
2024-06-26 00:21:53 +0300

    Add example for the CTOSS usage

--- Ctoss.Example/Program.cs
+++ Ctoss.Example/Program.cs
@@ -1,36 +1,35 @@
-﻿using Ctoss;
-using Ctoss.Example;
+﻿using Ctoss.Example;
+using Ctoss.Extensions;
 
-const string jsonString = """
-                          {
-                              "tin": {
-                                  "filterType": "date",
-                                  "condition1": {
-                                      "filterType": "date",
-                                      "type": "inRange",
-                                      "dateFrom": "10/10/2002",
-                                      "dateTo": "10/12/2020"
-                                  },
-                                  "conditions": [
-                                        {
-                                          "filterType": "date",
-                                          "type": "inRange",
-                                          "date": "10/10/2002",
-                                          "dateTo": "10/12/2020"
-                                        }
-                                   ]
-                              }
-                          }
-                          """;
-
-var filterBuilder = new FilterBuilder();
-var expr = filterBuilder.GetExpression<Entity>(jsonString);
-Console.WriteLine(expr);
-
-namespace Ctoss.Example
-{
-    class Entity
+const string jsonFilter =
+    """
     {
-        public DateTime Tin { get; set; }
+        "property": {
+            "filterType": "date",
+            "condition1": {
+                "filterType": "date",
+                "type": "inRange",
+                "dateFrom": "10/10/2002",
+                "dateTo": "10/12/2020"
+            },
+            "conditions": [
+                {
+                    "filterType": "date",
+                    "type": "inRange",
+                    "date": "10/10/2002",
+                    "dateTo": "10/12/2020"
+                }
+            ]
+        }
     }
+    """;
+
+var entities = ExampleEntityFaker.GetN(10).AsQueryable()
+    .WithFilter(jsonFilter) // <-- This is the extension method from the ctoss library
+    .ToList();
+
+Console.WriteLine("Filtered entities:");
+foreach (var entity in entities)
+{
+    Console.WriteLine(entity.Property);
 }
