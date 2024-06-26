3283bdc291a0a2407ba6e71330fee20656406d68
Bardin08 <vbardin810@gmail.com>
2024-06-26 11:39:09 +0300

    Add example and github actions

--- Ctoss.Example/Program.cs
+++ Ctoss.Example/Program.cs
@@ -24,6 +24,15 @@ const string jsonFilter =
     }
     """;
 
+/*
+ * The CTOSS gives you three overloads of the method WithFilter which evaluates a given filter and provides you with
+ * a filtering Expression<Func<T, bool>> fully compatible with IQueryable and EF.
+ *
+ * Overloads:
+ * - WithFilter<T>(this IQueryable<T> query, string jsonFilter)
+ * - WithFilter<T>(this IQueryable<T> query, string propertyName, Filter filter)
+ * - WithFilter<T>(this IQueryable<T> query, Dictionary<string, Filter> filters)
+ */
 var entities = ExampleEntityFaker.GetN(10).AsQueryable()
     .WithFilter(jsonFilter) // <-- This is the extension method from the ctoss library
     .ToList();
