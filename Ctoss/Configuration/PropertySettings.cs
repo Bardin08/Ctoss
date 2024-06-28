892856b0bccc6068540250984d763cf15cab61d5
Bardin08 <vbardin810@gmail.com>
2024-06-28 16:08:02 +0300

    Remove Required property option

--- Ctoss/Configuration/PropertySettings.cs
+++ Ctoss/Configuration/PropertySettings.cs
@@ -6,14 +6,6 @@
 public class PropertySettings
 {
     /// <summary>
-    /// Gets or sets a value indicating whether the property is required.
-    /// </summary>
-    /// <value>
-    /// <c>true</c> if the property is required; otherwise, <c>false</c>.
-    /// </value>
-    public bool Required { get; set; }
-
-    /// <summary>
     /// Gets or sets a value indicating whether the case should be ignored when resolving the property.
     /// </summary>
     /// <value>
