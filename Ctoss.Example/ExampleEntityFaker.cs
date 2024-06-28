ddf73b17e8fd8e23d14f4ad353af24fe4b6eee1d
Bardin08 <vbardin810@gmail.com>
2024-06-28 15:27:38 +0300

    Add numeric examples

--- Ctoss.Example/ExampleEntityFaker.cs
+++ Ctoss.Example/ExampleEntityFaker.cs
@@ -5,7 +5,17 @@ namespace Ctoss.Example;
 internal class ExampleEntityFaker : Faker<ExampleEntity>
 {
     private static readonly Faker<ExampleEntity> Faker = new ExampleEntityFaker()
-        .RuleFor(x => x.Property, f => f.Date.Between(new DateTime(1970, 1, 1), new DateTime(2024, 6, 6)));
+        .RuleFor(x => x.Property, f => f.Date.Between(new DateTime(1970, 1, 1), new DateTime(2024, 6, 6)))
+        .RuleFor(x => x.Property2, _ => TimeSpan.FromDays(356 * 5));
 
     public static IQueryable<ExampleEntity> GetN(int n) => Faker.GenerateLazy(n).AsQueryable();
+}
+
+internal class ExampleNumericEntityFaker : Faker<ExampleNumericEntity>
+{
+    private static readonly Faker<ExampleNumericEntity> Faker = new ExampleNumericEntityFaker()
+        .RuleFor(x => x.A, f => f.Random.Int(0, 100))
+        .RuleFor(x => x.B, f => f.Random.Int(0, 100));
+
+    public static IQueryable<ExampleNumericEntity> GetN(int n) => Faker.GenerateLazy(n).AsQueryable();
 }
--- Ctoss.Example/Program.cs
+++ Ctoss.Example/Program.cs
@@ -1,10 +1,19 @@
-﻿using Ctoss.Example;
+﻿using Ctoss.Configuration;
+using Ctoss.Example;
 using Ctoss.Extensions;
+
+CtossSettingsBuilder.Create()
+    .Entity<ExampleEntity>()
+    .Property("Property", x => x.Property + x.Property2, p => { p.IgnoreCase = true; })
+    .Apply()
+    .Entity<ExampleNumericEntity>()
+    .Property("virtual", x => x.A + x.B)
+    .Apply();
 
 const string jsonFilter =
     """
     {
-        "PROPERTY": {
+        "PrOpErTy": {
             "filterType": "date",
             "condition1": {
                 "filterType": "date",
@@ -24,6 +33,28 @@ const string jsonFilter =
     }
     """;
 
+const string jsonNumericFilter =
+    """
+    {
+        "virtual": {
+            "filterType": "number",
+            "condition1": {
+                "filterType": "number",
+                "type": "inRange",
+                "filter": "10",
+                "filterTo": "70"
+            },
+            "conditions": [
+                {
+                    "filterType": "number",
+                    "type": "GreaterThan",
+                    "filter": "10"
+                }
+            ]
+        }
+    }
+    """;
+
 /*
  * The CTOSS gives you three overloads of the method WithFilter which evaluates a given filter and provides you with
  * a filtering Expression<Func<T, bool>> fully compatible with IQueryable and EF.
@@ -33,12 +64,17 @@ const string jsonFilter =
  * - WithFilter<T>(this IQueryable<T> query, string propertyName, Filter filter)
  * - WithFilter<T>(this IQueryable<T> query, Dictionary<string, Filter> filters)
  */
-var entities = ExampleEntityFaker.GetN(10).AsQueryable()
+var entities = ExampleEntityFaker.GetN(100).AsQueryable()
     .WithFilter(jsonFilter) // <-- This is the extension method from the ctoss library
     .ToList();
 
 Console.WriteLine("Filtered entities:");
-foreach (var entity in entities)
-{
-    Console.WriteLine(entity.Property);
-}
+foreach (var entity in entities) Console.WriteLine(entity.Property);
+
+Console.WriteLine("\nNumeric entities:");
+
+var numericEntities = ExampleNumericEntityFaker.GetN(100).AsQueryable()
+    .WithFilter(jsonNumericFilter) // <-- This is the extension method from the ctoss library
+    .ToList();
+
+foreach (var entity in numericEntities) Console.WriteLine($"A: {entity.A}, B: {entity.B}");
