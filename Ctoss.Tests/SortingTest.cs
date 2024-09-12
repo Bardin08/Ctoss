8e3e6aea30ccd60ef587dc33b2ce9c709cf0c7bc
AndriiPodkolzin <podkolzzzin1996@gmail.com>
2024-09-12 17:29:54 +0300

    Add test for sorting

---
+++ Ctoss.Tests/SortingTest.cs
@@ -0,0 +1,29 @@
+﻿using Ctoss.Builders.Filters;
+using Ctoss.Extensions;
+using Ctoss.Models;
+using Ctoss.Models.AgGrid;
+using Ctoss.Models.Enums;
+using Ctoss.Tests.Models;
+
+namespace Ctoss.Tests;
+
+public class SortingTest
+{
+    private readonly FilterBuilder _filterBuilder = new();
+
+    private readonly List<TestEntity> _testEntities =
+    [
+        new TestEntity(numericProperty: 10, stringProperty: "abc", dateTimeProperty: new DateOnly(2022, 1, 1)),
+        new TestEntity(numericProperty: 20, stringProperty: "def", dateTimeProperty: new DateOnly(2023, 2, 2)),
+        new TestEntity(numericProperty: 30, stringProperty: "ghi", dateTimeProperty: new DateOnly(2024, 3, 3))
+    ];
+
+
+    [Fact]
+    public void Enumerable_SingleProperty_Success()
+    {
+        var filter = new AgGridQuery(0, 100, [new Sorting() { Property = "DateTimeProperty", Order = SortingOrder.Desc }], []);
+        var result = _testEntities.Apply(filter);
+        Assert.True(result.Rows.SequenceEqual(_testEntities.OrderByDescending(x => x.DateTimeProperty)));
+    }
+}
